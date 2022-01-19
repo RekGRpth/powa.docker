f391dedb952eafa2c9943f1a6cdd9ca8e5e71093
RekGRpth <rekgrpth@gmail.com>
2022-01-19 15:00:11 +0500

    fix

--- bin/docker_build_alpine.sh
+++ bin/docker_build_alpine.sh
@@ -6,16 +6,16 @@ apk add --no-cache --virtual .build-deps \
     g++ \
     gcc \
     git \
+    libpq-dev \
     linux-headers \
     make \
     musl-dev \
     pcre2-dev \
     pcre-dev \
-    postgresql-dev \
-    py3-pip \
-    py3-wheel \
-    python3-dev \
     py3-greenlet \
+    py3-pip \
     py3-psycopg2 \
     py3-sqlalchemy \
+    py3-wheel \
+    python3-dev \
 ;
