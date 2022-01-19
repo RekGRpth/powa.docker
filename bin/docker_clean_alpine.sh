4bb3838d1a344472da439e132d186394811229c1
RekGRpth <rekgrpth@gmail.com>
2022-01-19 14:49:21 +0500

    fix

--- bin/docker_clean_alpine.sh
+++ bin/docker_clean_alpine.sh
@@ -2,11 +2,10 @@
 
 cd /
 apk add --no-cache --virtual .powa-rundeps \
-        py3-greenlet \
-        py3-psycopg2 \
-        py3-sqlalchemy \
-        su-exec \
-        uwsgi-python3 \
+    py3-greenlet \
+    py3-psycopg2 \
+    py3-sqlalchemy \
+    uwsgi-python3 \
     $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do test ! -e "/usr/local/lib/$lib" && echo "so:$lib"; done) \
 ;
 find /usr/local/bin -type f -exec strip '{}' \;
